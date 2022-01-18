2c88360736db2e2a624b793106c5c07e035995f0
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-18 08:49:04 +0100

    Logische Aussagen hinzugefügt

--- script_01.js
+++ script_01.js
@@ -2,3 +2,28 @@
 
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assignment//
+
+const ageJohn = 31;
+const ageMark = 30;
+
+//Deklaration
+
+let isJohnOlder, isJohnEqual; //CamelCase fängt mit "is" an weist auf einen Wahrheitswert hin// 
+
+//Test Logische Aussage
+
+isJohnOlder = (ageJohn > ageMark); 
+isJohnEqual = (ageJohn == ageMark); //bei equal Vergleichen immer mit == vergleichen//
+
+//Ausgabe//
+
+console.log("ageJohn : " + ageJohn);
+console.log("ageMark : " + ageMark);
+console.log("isJohnOlder : " + isJohnOlder);
+console.log("isjohnEqual : " + isJohnEqual);
+
+
+
+
+
